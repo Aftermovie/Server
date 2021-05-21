f7f961bf0ac21abb6622edb48ef46f9df4fee2f8
RYS <ryeongseok1125@gmail.com>
2021-05-21 09:24:46 +0900

    Add Movies models

--- movies/models.py
+++ movies/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    poster_path = models.CharField(max_length=200)
 
-class Comment(models.Model):
-    pass
 
 class Review(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    rank = models.IntegerField()
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
+class MovieComment(models.Model):
+    content = models.CharField(max_length=100)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='comment')
+    rank = models.IntegerField()
+
+
+class ReviewComment(models.Model):
+    content = models.CharField(max_length=100)
+    review_id = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comment')
