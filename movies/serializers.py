4f3b1c5ade49251c2f0fab223f30dbe2cd83aff1
RYS <ryeongseok1125@gmail.com>
2021-05-21 09:26:03 +0900

    Add serializer.py

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,23 +1,59 @@
 from rest_framework import serializers
-from .models import Movie, Comment, Review
+from .models import Movie, ReviewComment, Review, MovieComment
 
 
-class MovieSerializer(serializers.ModelSerializer):
-
+class MoviesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields= '__all__'
+        fields = '__all__'
 
 
-class CommentSerializer(serializers.ModelSerializer):
-
+class ReviewCommentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Comment
-        fields= '__all__'
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('review_id',)
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-
+    comment = ReviewCommentSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Review
-        fields= '__all__'
+        fields = '__all__'
+        read_only_fields = ('movie_id',)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    review_set = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class ReviewsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('movie_id',)
+
+
+class ReviewCommentsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewComment
+        fields = '__all__'
+        read_only_fields = ('review_id',)
+
+
+class MovieCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('movie_id',)
+
+
+class MovieCommentsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieComment
+        fields = '__all__'
+        read_only_fields = ('movie_id',)
