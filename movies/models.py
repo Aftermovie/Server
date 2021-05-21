f6d5038ef3b8ce0c8b541b4bd903f73936cda54e
RYS <ryeongseok1125@gmail.com>
2021-05-21 09:34:46 +0900

    Modify models

--- movies/models.py
+++ movies/models.py
@@ -13,14 +13,17 @@ class Review(models.Model):
     content = models.TextField()
     rank = models.IntegerField()
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class MovieComment(models.Model):
     content = models.CharField(max_length=100)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='comment')
     rank = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class ReviewComment(models.Model):
     content = models.CharField(max_length=100)
     review_id = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comment')
+    created_at = models.DateTimeField(auto_now_add=True)
