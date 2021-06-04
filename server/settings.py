a7b6def4aae532a41ff5dadf2cfe021580e43f80
rhyuys115 <ryeongseok1125@gmail.com>
2021-06-04 15:29:15 +0900

    DB 설정 수정

--- server/settings.py
+++ server/settings.py
@@ -90,6 +90,9 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {
+}
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
