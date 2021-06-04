8bb5cae66284844915fa330712bb294afddeae14
rhyuys115 <ryeongseok1125@gmail.com>
2021-06-04 19:31:20 +0900

    CORS 수정

--- server/settings.py
+++ server/settings.py
@@ -62,9 +62,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+# ]
 
 ROOT_URLCONF = 'server.urls'
 
