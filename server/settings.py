3c20a8a89fb2de897afe3a899eeccd3e6133a0c1
rhyuys115 <ryeongseok1125@gmail.com>
2021-06-06 11:22:39 +0900

    CORS Method 수정

--- server/settings.py
+++ server/settings.py
@@ -70,6 +70,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
